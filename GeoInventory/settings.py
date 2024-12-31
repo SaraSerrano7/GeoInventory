91449f23e2853cd9a3d96d904ce30f2128cf5904
SaraSerrano7 <sasecu@gmail.com>
2024-12-31 18:30:33 +0100

    changed secret key and added config folder to gitignore

--- GeoInventory/settings.py
+++ GeoInventory/settings.py
@@ -47,8 +47,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Files',
-    'Api',
+    'Files_',
+    'Api_',
 ]
 
 MIDDLEWARE = [
