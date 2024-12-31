51949e72a16321f41816593acee61778b36a05f8
SaraSerrano7 <sasecu@gmail.com>
2024-12-31 18:41:02 +0100

    fixed installed apps name typos

--- GeoInventory/settings.py
+++ GeoInventory/settings.py
@@ -47,8 +47,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Files_',
-    'Api_',
+    'Files',
+    'Api',
 ]
 
 MIDDLEWARE = [
