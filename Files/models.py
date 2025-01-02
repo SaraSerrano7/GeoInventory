8d97bef630678a94202f3ce774c33bd597391ac6
SaraSerrano7 <sasecu@gmail.com>
2025-01-02 13:24:33 +0100

    - added models for Files app:

--- Files/models.py
+++ Files/models.py
@@ -1,3 +1,38 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.utils.timezone import now
+
 
 # Create your models here.
+
+class Management_info(models.Model):
+    creator = models.ForeignKey(User, default='deleted user', on_delete=models.SET_DEFAULT)
+    created_at = models.DateTimeField(default=now())
+    deleted = models.BooleanField()
+    deleted_at = models.DateTimeField(default=now())
+
+
+class Team(models.Model):
+    name = models.CharField(primary_key=True, max_length=50)
+    info = models.ManyToManyField(Management_info, related_name='info')
+
+
+ROLES_CHOICES = [
+    ('GUEST', 'G'),  # user can log in the app
+    ('VIEWER', 'V'),  # user can view files
+    ('CREATOR', 'C'),  # user can create and edit files
+    ('OWNER', 'O'),  # user can delete files
+    ('ADMIN', 'A'),  # user can create, edit and delete teams and projects
+]
+
+
+class Role(models.Model):
+    role_name = models.CharField()
+    info = models.ManyToManyField(Management_info, related_name='info')
+
+
+class Membership(models.Model):
+    member = models.ForeignKey(User, default='deleted user', on_delete=models.CASCADE())
+    user_team = models.ForeignKey(User, default='deleted team', on_delete=models.SET_DEFAULT)
+    user_role = models.ForeignKey(Role, default=ROLES_CHOICES[0], on_delete=models.SET_DEFAULT)
+    info = models.ManyToManyField(Management_info, related_name='info')
--- Management/models.py
+++ Management/models.py
@@ -4,8 +4,8 @@ from django.db import models
 # Create your models here.
 
 GLOBAL_ROLES_CHOICES = [
-    ('superadmin', 'superadmin'),
     ('regular', 'regular'),
+    ('superadmin', 'superadmin'),
 ]
 
 
@@ -14,5 +14,5 @@ class Global_role(models.Model):
 
 
 class Global_membership(models.Model):
-    user_type = models.ForeignKey(Global_role, default='regular', on_delete=models.SET_DEFAULT)
-    related_user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user_type = models.ForeignKey(Global_role, default=GLOBAL_ROLES_CHOICES[0], on_delete=models.SET_DEFAULT)
+    related_user = models.ForeignKey(User, default='deleted user', on_delete=models.SET_DEFAULT)
