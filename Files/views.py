0b3cd093ef99b6ebaa1629d1bef44d1c4d9c4ce2
SaraSerrano7 <sasecu@gmail.com>
2025-01-03 13:22:32 +0100

    added login logout

--- Files/views.py
+++ Files/views.py
@@ -4,8 +4,10 @@ Files' app views to develop file management functions
 
 # Create your views here.
 from django.views.generic import TemplateView
+from django.contrib.auth.decorators import login_required
 
 
+# @login_required
 class HomePageView(TemplateView):
     """
     Main view where the user interacts with the Files app
