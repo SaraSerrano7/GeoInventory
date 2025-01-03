594ec0461a4ef460cbfa19fd4537251a0dcca1ca
SaraSerrano7 <sasecu@gmail.com>
2025-01-03 17:13:01 +0100

    decoupled header, file explorer and data viewer into different components. now main view requires to be logged in to access

--- Files/views.py
+++ Files/views.py
@@ -5,7 +5,7 @@ Files' app views to develop file management functions
 # Create your views here.
 from django.views.generic import TemplateView
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 # @login_required
@@ -13,4 +13,6 @@ def homepageView(request):
     """
     Main view where the user interacts with the Files app
     """
+    if not request.user.is_authenticated:
+        return redirect('login')
     return render(request, 'Files/home.html')
