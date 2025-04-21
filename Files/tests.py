5833813c4d1117bb87358e9d553c31d66584ea48
SaraSerrano7 <sasecu@gmail.com>
2025-04-21 21:29:46 +0200

    made somo executions with pymongo, directly with bulk. fixed test to stop using orm

--- Files/tests.py
+++ Files/tests.py
@@ -6,9 +6,11 @@ import os
 from datetime import datetime
 
 import requests
+from bson import ObjectId
 from django.contrib.auth.models import User
 from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import TestCase
+from pymongo import MongoClient
 
 # Create your tests here.
 from Files.models import Role, Team, Membership, Project, Assignations, GeoJSONFeature, GeoJSONFeatureProperties
@@ -40,6 +42,16 @@ class SimpleTest(TestCase):
 
     @classmethod
     def setUpTestData(cls):
+
+        # user = mongo_db['auth_user'].insert_one({
+        #     'id': ObjectId,
+        #     'username': "creatorUser",
+        #     'email': "creatorUser@example.com",
+        #     'password': "jamon",
+        #     'is_superuser': False,
+        #     'is_staff': False
+        # })
+
         user = User.objects.create_user(
             username="creatorUser",
             email="creatorUser@example.com",
@@ -47,42 +59,104 @@ class SimpleTest(TestCase):
             is_superuser=False,
             is_staff=False
         )
+        mongo_db = MongoClient('mongodb://localhost:27017/')['test_geoinventory']
 
-        cls.user = user
+        # user_id = user.inserted_id
+        cls.user = user.id
 
-        role = Role.objects.create(
-            role_name="creator",
-            creator=user,
-        )
+        digitalresource_result = mongo_db['Files_digitalresource'].insert_one({
+            "id": ObjectId(),
+            "creator_id": user.id,
+            "created_at": datetime.now(),
+        })
 
-        cls.role = role
+        role = mongo_db['Files_role'].insert_one({
+            "digitalresource_ptr_id": digitalresource_result.inserted_id,
+            'role_name':  "creator",
+            'creator_id': user.id,
+        })
 
-        team = Team.objects.create(
-            name="team_patata",
-        )
+        # role = Role.objects.create(
+        #     role_name="creator",
+        #     creator=user,
+        # )
+        role_id = role.inserted_id
 
-        cls.team = team
+        # team = Team.objects.create(
+        #     name="team_patata",
+        # )
 
-        membership = Membership.objects.create(
-            member=user,
-            user_role=role,
-            user_team=team
-        )
+        cls.role = role_id
 
-        cls.membership = membership
+        digitalresource_result = mongo_db['Files_digitalresource'].insert_one({
+            "id": ObjectId(),
+            "creator_id": user.id,
+            "created_at": datetime.now(),
+        })
+        team = mongo_db['Files_team'].insert_one({
+            "digitalresource_ptr_id": digitalresource_result.inserted_id,
+            'name': "team_patata",
+        })
+        team_id = team.inserted_id
 
-        project = Project.objects.create(
-            name="proyecto_cultivos_herbaceos",
-        )
+        cls.team = team_id
+        cls.team_name = 'team_patata'
 
-        cls.project = project
+        # membership = Membership.objects.create(
+        #     member=user,
+        #     user_role=role,
+        #     user_team=team
+        # )
 
-        assignation = Assignations.objects.create(
-            assignated_project=project,
-            assignated_team=team,
-        )
+        digitalresource_result = mongo_db['Files_digitalresource'].insert_one({
+            "id": ObjectId(),
+            "creator_id": user.id,
+            "created_at": datetime.now(),
+        })
+        membership = mongo_db['Files_membership'].insert_one({
+            "digitalresource_ptr_id": digitalresource_result.inserted_id,
+            'member_id': user.id,
+            'user_role_id': role_id,
+            'user_team_id': team_id
+        })
+        membership_id = membership.inserted_id
+        cls.membership = membership_id
 
-        cls.assignation = assignation
+        # project = Project.objects.create(
+        #     name="proyecto_cultivos_herbaceos",
+        # )
+
+        digitalresource_result = mongo_db['Files_digitalresource'].insert_one({
+            "id": ObjectId(),
+            "creator_id": user.id,
+            "created_at": datetime.now(),
+        })
+        project = mongo_db['Files_project'].insert_one({
+            "digitalresource_ptr_id": digitalresource_result.inserted_id,
+            'name': "proyecto_cultivos_herbaceos",
+        })
+        project_id = project.inserted_id
+        cls.project = project_id
+        cls.project_name = 'proyecto_cultivos_herbaceos'
+
+        # assignation = Assignations.objects.create(
+        #     assignated_project=project,
+        #     assignated_team=team,
+        # )
+
+        digitalresource_result = mongo_db['Files_digitalresource'].insert_one({
+            "id": ObjectId(),
+            "creator_id": user.id,
+            "created_at": datetime.now(),
+        })
+        assignation = mongo_db['Files_assignation'].insert_one({
+            "digitalresource_ptr_id": digitalresource_result.inserted_id,
+            'assignated_project': project_id,
+            'assignated_team': team_id,
+        })
+        assignation_id = assignation.inserted_id
+
+        cls.assignation = assignation_id
 
     def test_sample_creation(self):
         files = [f for f in os.listdir(self.test_folder) if f.endswith(".geojson")]
@@ -103,9 +177,9 @@ class SimpleTest(TestCase):
 
                 form_data = {
                     "fileName": filename,
-                    "project": self.project.name,
-                    "location": self.project.name,
-                    "teams": json.dumps([self.team.name]),
+                    "project": self.project_name,
+                    "location": self.project_name,
+                    "teams": json.dumps([self.team_name]),
                     "categories": json.dumps([]),
                 }
 
